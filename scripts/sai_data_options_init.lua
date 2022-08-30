ed77f35916a0f5e1aee1dbab852b481bc93d6735
ZarestiaDev <6247787+ZarestiaDev@users.noreply.github.com>
2022-08-30 15:19:39 +0200

    Fix options menu for SFRPG

--- scripts/sai_data_options_init.lua
+++ scripts/sai_data_options_init.lua
@@ -1,8 +1,17 @@
 function onInit()
-	registerOptions();
+	if User.getRulesetName() == "SFRPG" then
+		registerOptionsSFRPG();
+	else
+		registerOptionsDND();
+	end
 end
 
-function registerOptions()
+function registerOptionsDND()
 	OptionsManager.registerOption2("SAIO",true, "option_header_client", "option_label_SAIO", "option_entry_cycler", 
 		{ labels = "option_val_components|option_val_range", values = "components|range", baselabel = "option_val_off", baseval = "off", default = "off" });
+end
+
+function registerOptionsSFRPG()
+	OptionsManager.registerOption2("SAIO",true, "option_header_client", "option_label_SAIO", "option_entry_cycler", 
+		{ labels = "option_val_range", values = "range", baselabel = "option_val_off", baseval = "off", default = "off" });
 end
